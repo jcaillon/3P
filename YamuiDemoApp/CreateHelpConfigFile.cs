72004ac4eb7b49434e042f3c2336a690701bd783
Julien Caillon <julien.caillon@gmail.com>
2015-11-06 19:17:20 +0100

    to github

--- YamuiDemoApp/CreateHelpConfigFile.cs
+++ YamuiDemoApp/CreateHelpConfigFile.cs
@@ -15,39 +15,69 @@ namespace YamuiDemoApp {
         public static void Do() {
 
             var output = new StringBuilder();
-            var indexContent = File.ReadAllText(@"C:\Work\3PA_side\ProgressFiles\help_tooltip\extracted\toc.hhc", Encoding.Default);
+            var log = new StringBuilder();
+            var indexContent = File.ReadAllText(@"C:\Work\3PA_side\ProgressFiles\help_tooltip\extracted\index.hhk", Encoding.Default);
 
-            foreach (var items in File.ReadAllLines(@"C:\Work\3PA\3PA\Data\keywords.data", Encoding.Default).Select(line => line.Split('\t')).Where(items => items.Count() == 4)) {
+            foreach (var items in File.ReadAllLines(@"C:\Work\3PA_side\ProgressFiles\help_tooltip\keywords.data", Encoding.Default).Select(line => line.Split('\t')).Where(items => items.Count() == 4)) {
 
                 string keyword = items[0];
 
-                var regex = new Regex("<param name=\"Name\" value=\"" + keyword + " " + items[1] + "[^\"]*\">[^<]*<param name=\"Local\" value=\"([^\"]*)\">", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+                var regex = new Regex("<param name=\"Name\" value=\"(" + keyword + " " + items[1] + "[^\"]*)\">[^<]*(?:<param name=\"Name\" value=\"([^\"]*)\">[^<]*)?<param name=\"Local\" value=\"([^\"]*)\">", RegexOptions.Singleline | RegexOptions.IgnoreCase);
                 var m = regex.Match(indexContent);
                 if (m.Success) {
-                    //MessageBox.Show(@"normal regex for " + keyword + " " + items[1] + " :" + m.Groups[1].Value);
-                    // m.Groups[1].Value = "11dvref-EF.10.16.html"
-                    if (File.Exists(@"C:\Work\3PA_side\ProgressFiles\help_tooltip\extracted\" + m.Groups[1].Value))
-                        output.AppendLine(keyword + " " + items[1] + "\t" + ExtractFromHtml(m.Groups[1].Value));
+                    var filename = m.Groups[3].Value;
+                    if (filename.Contains("#"))
+                        filename = filename.Substring(0, filename.IndexOf('#'));
+                    if (File.Exists(@"D:\temp\lgrfeng\" + filename))
+                        output.AppendLine(keyword + " " + items[1] + "\t" + ExtractFromHtml(filename));
+                    else
+                        log.AppendLine("file not found for :" + keyword + " " + items[1]);
                 } else {
+                    if (items[1] != "Statement" && items[1] != "Appbuilder" && items[1] != "Abbreviation" && items[1] != "Option" && items[1] != "Preprocessor" && items[1] != "Type") {
+
+                        string newmatchtext = keyword;
+
+                        if (items[1] == "Method")
+                            newmatchtext = keyword + @"[\W]+method";
+
+                        regex = new Regex("<param name=\"Name\" value=\"(" + newmatchtext + "[^\"]*)\">[^<]*(?:<param name=\"Name\" value=\"([^\"]*)\">[^<]*)?<param name=\"Local\" value=\"([^\"]*)\">", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+                        m = regex.Match(indexContent);
+                        if (m.Success) {
+                            var filename = m.Groups[3].Value;
+                            if (filename.Contains("#"))
+                                filename = filename.Substring(0, filename.IndexOf('#'));
+                            if (File.Exists(@"D:\temp\lgrfeng\" + filename)) {
+                                output.AppendLine(keyword + " " + items[1] + "\t" + ExtractFromHtml(filename));
+                            } else
+                                log.AppendLine("file not found for :" + keyword + " " + items[1]);
+                        } else {
+                            log.AppendLine(keyword + " " + items[1]);
+                        }
+                    }
+                    /*
                     // try to match only the keyword?
-                    regex = new Regex("<param name=\"Name\" value=\"" + keyword + "[^\"]*\">[^<]*<param name=\"Local\" value=\"([^\"]*)\">", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+                    regex = new Regex("<param name=\"Name\" value=\"" + keyword + " [^\"]*" + items[1] + "[^\"]*\">[^<]*<param name=\"Local\" value=\"([^\"]*)\">", RegexOptions.Singleline | RegexOptions.IgnoreCase);
                     m = regex.Match(indexContent);
                     if (m.Success) {
                         //MessageBox.Show(@"degraded regex for " + keyword + " :" + m.Groups[1].Value);
-                        if (File.Exists(@"C:\Work\3PA_side\ProgressFiles\help_tooltip\extracted\" + m.Groups[1].Value))
+                        if (File.Exists(@"D:\temp\lgrfeng\" + m.Groups[1].Value))
                             output.AppendLine(keyword + " " + items[1] + "\t" + ExtractFromHtml(m.Groups[1].Value));
                     }
-                }  
+                    */
+                }
             }
 
-            File.WriteAllText(@"C:\Work\3PA\3PA\Data\keywordsHelp.data", output.ToString(), Encoding.Default);
+            File.WriteAllText(@"C:\Work\3PA_side\ProgressFiles\help_tooltip\keywords.log", log.ToString(), Encoding.Default);
+            File.WriteAllText(@"C:\Work\3PA_side\ProgressFiles\help_tooltip\keywordsHelp.data", output.ToString(), Encoding.Default);
         }
+
 
         private static string ExtractFromHtml(string htmlName) {
             string paragraph = "";
             var synthax = new StringBuilder();
             int status = 0;
-            foreach (var lines in File.ReadAllLines(@"C:\Work\3PA_side\ProgressFiles\help_tooltip\extracted\" + htmlName, Encoding.Default)) {
+            bool first = true;
+            foreach (var lines in File.ReadAllLines(@"D:\temp\lgrfeng\" + htmlName, Encoding.Default)) {
                 var line = lines.Trim();
                 if (status == 0) {
                     if (line.StartsWith("<div class=\"paragraph\">")) {
@@ -57,17 +87,23 @@ namespace YamuiDemoApp {
                 } else if (status == 1) {
                     if (line.StartsWith("<div class=\"paramete"))
                         status = -1;
-                        
-                    if (line.StartsWith("<table class=\"table_"))
+
+                    if (line.StartsWith("<table class=\"table_")) {
                         status++;
+                        first = true;
+                    }
                 } else if (status == 2) {
                     if (line.StartsWith("</table>")) {
                         status = 1;
                         synthax.Append("\t");
                     }
 
-                    if (line.StartsWith("<pre class=\"cell_003acode\">"))
+                    if (line.StartsWith("<pre class=\"cell_003acode\">")) {
+                        if (!first) synthax.Append("<br>");
+                        if (first) first = false;
                         synthax.Append(ClearTags(line));
+
+                    }
                 }
             }
             return paragraph + "\t" + synthax;
@@ -76,5 +112,6 @@ namespace YamuiDemoApp {
         private static string ClearTags(string input) {
             return Regex.Replace(input.Replace(@"<br />", "~n"), "<.*?>", string.Empty).Replace("~n", "<br>").Replace("\t", " ");
         }
+    
     }
 }
